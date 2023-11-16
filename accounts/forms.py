4f3bcd4dbcd4891ab80672edd426ea547705c0b8
LuchoHamze <lucianohamze0@gmail.com>
2023-11-16 00:59:01 -0300

    Agregado de Biografia y Avatar en la edicion de Perfil del usuario

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,14 +3,17 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 
 class MiFormularioDeCreacion(UserCreationForm):
+    username = forms.CharField(label= "Nombre de Usuario")
     email = forms.EmailField()
     password1 = forms.CharField (label= "Contrasena", widget= forms.PasswordInput)
     password2 = forms.CharField (label= "Repetir Contrasena", widget= forms.PasswordInput)
+    first_name = forms.CharField(label= "Nombre")
+    last_name =  forms.CharField(label= "Apellido")
     
     class Meta:
         
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
         help_text = {key: " " for key in fields}
         
 class EdicionPerfil(UserChangeForm):
@@ -18,7 +21,9 @@ class EdicionPerfil(UserChangeForm):
     email = forms.EmailField(label= "Cambiar Correo electrónico", required=False)
     first_name = forms.CharField(label= "Nombre", required=False)
     last_name =  forms.CharField(label= "Apellido", required=False)
+    biografia = forms.CharField(label= "Biografia", required=False, widget=forms.Textarea)
+    avatar = forms.ImageField(required=False)
     
     class Meta:
         model = User
-        fields = ["email", "first_name", "last_name"]
+        fields = ["email", "first_name", "last_name", "biografia"]
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login as django_login
 from accounts.forms import MiFormularioDeCreacion, EdicionPerfil
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
+from accounts.models import DatosExtra
 
 def login (request):
     
@@ -18,6 +19,8 @@ def login (request):
             user = authenticate(username=usuario, password=contra)
             
             django_login(request, user)
+            
+            DatosExtra.objects.get_or_create(user=request.user)
             
             return redirect("inicio")  
         
@@ -38,14 +41,31 @@ def registro (request):
     return render(request, "accounts/registro.html", {"formulario_de_registro": formulario_de_registro})
 
 def editar_perfil (request):
-    formulario_de_perfil = EdicionPerfil(instance=request.user)
+    
+    datos_extra = request.user.datosextra
+    
+    formulario_de_perfil = EdicionPerfil(initial={"biografia": datos_extra.biografia, "avatar": datos_extra.avatar}, instance=request.user)
     
     if request.method == "POST":
-        formulario_de_perfil = EdicionPerfil(request.POST, instance= request.user)
+        formulario_de_perfil = EdicionPerfil(request.POST, request.FILES, instance= request.user)
         
         if formulario_de_perfil.is_valid():
             
+            nueva_biografia = formulario_de_perfil.cleaned_data.get("biografia")
+            nuevo_avatar = formulario_de_perfil.cleaned_data.get("avatar")
+            
+            if nueva_biografia:
+                datos_extra.biografia = nueva_biografia
+            if nuevo_avatar:
+                datos_extra.avatar = nuevo_avatar
+                
+                
+            datos_extra.save()
+
+            
             formulario_de_perfil.save()
+            
+            return redirect("editar_perfil")
     
     return render(request, "accounts/editar_perfil.html", {"formulario_de_perfil": formulario_de_perfil})
 
